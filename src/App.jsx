7e2b8db063b7181af0c3f4b33444af1f952d9efd
Huzaifa Karim <karimhuzaifa590@gmail.com>
2025-08-24 19:46:34 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
-import './App.css';
-import Header from './components/common/Header';
-import Nav from './components/common/Nav';
-import Main from './components/Main';
-import Footer from './components/common/Footer';
-import { BrowserRouter as Router } from 'react-router-dom';
+import "./App.css";
+import Header from "./components/common/Header";
+import Nav from "./components/common/Nav";
+import Main from "./components/Main";
+import Footer from "./components/common/Footer";
+import { BrowserRouter as Router } from "react-router-dom";
+import { CartProvider } from "./components/pages/MenuPage";
 
 function App() {
   return (
     <Router>
-      <Header />
-      <Nav />
-      <Main />
-      <Footer />
+      <CartProvider>
+        <Header />
+        <Nav />
+        <Main />
+        <Footer />
+      </CartProvider>
     </Router>
   );
 }
