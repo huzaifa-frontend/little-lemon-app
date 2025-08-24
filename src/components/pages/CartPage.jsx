9388ead971dd097282885c1f0afeff534d80c6cb
Huzaifa Karim <karimhuzaifa590@gmail.com>
2025-08-24 15:45:52 +0500

    Create CartPage.jsx

---
+++ src/components/pages/CartPage.jsx
@@ -0,0 +1,10 @@
+function CartPage() {
+  return (
+    <main className="cart-page">
+      <h1>Your Cart</h1>
+      <p>Review items in your cart and proceed to checkout.</p>
+    </main>
+  );
+}
+
+export default CartPage;
