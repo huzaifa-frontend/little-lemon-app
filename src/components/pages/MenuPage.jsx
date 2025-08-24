ba441d7bb328b0b3c17ee0a53dfe726edfa2301e
Huzaifa Karim <karimhuzaifa590@gmail.com>
2025-08-24 15:46:20 +0500

    Create MenuPage.jsx

---
+++ src/components/pages/MenuPage.jsx
@@ -0,0 +1,10 @@
+function MenuPage() {
+  return (
+    <main className="menu-page">
+      <h1>Our Menu</h1>
+      <p>Explore our delicious Mediterranean dishes.</p>
+    </main>
+  );
+}
+
+export default MenuPage;
