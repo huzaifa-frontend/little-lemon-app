ca5bbfd39ca21f84006552bc495f09396e484d38
Huzaifa Karim <karimhuzaifa590@gmail.com>
2025-08-24 15:46:23 +0500

    Create ProfilePage.jsx

---
+++ src/components/pages/ProfilePage.jsx
@@ -0,0 +1,10 @@
+function ProfilePage() {
+  return (
+    <main className="profile-page">
+      <h1>Your Profile</h1>
+      <p>Manage your account information and orders here.</p>
+    </main>
+  );
+}
+
+export default ProfilePage;
